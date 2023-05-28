b3815f22589166bc2e9fb3f6fcf996c9d4efa749
goutam-23 <69918713+goutam-23@users.noreply.github.com>
2023-05-28 23:32:21 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Form from './Form';
+import WeatherDisplay from './WeatherDisplay';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [weatherData, setWeatherData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async city => {
+    try {
+      setLoading(true);
+      const response = await fetch(
+        `https://api.weatherapi.com/v1/current.json?key=a79d605460c34af3931164613232805&q=${city}`
+      );
+      if (!response.ok) {
+        throw new Error('Failed to fetch weather data.');
+      }
+      const data = await response.json();
+      setWeatherData(data);
+      setError(null);
+      setLoading(false);
+    } catch (error) {
+      setError(error.message);
+      setLoading(false);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1>Weather App</h1>
+      <Form onSubmit={handleSubmit} />
+      {loading && <p>Loading...</p>}
+      {error && <p>{error}</p>}
+      {weatherData && <WeatherDisplay weatherData={weatherData} />}
     </div>
   );
-}
+};
 
 export default App;
